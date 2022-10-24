2feac1d59b35ae74264fd3df64d6111e6c35988e
Alina200207 <alina_m2002@list.ru>
2022-10-24 18:20:08 +0500

    добавила вид профиля

--- app/src/main/java/com/example/xiinder/CardsFragment.kt
+++ app/src/main/java/com/example/xiinder/CardsFragment.kt
@@ -5,6 +5,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.xiinder.R
@@ -20,8 +23,13 @@ class CardsFragment : Fragment() {
     ): View? {
         val binding = FragmentCardsBinding.inflate(inflater)
 
-        binding.photosLinear.adapter= this.context?.let { CardsAdapter(it,DataSetForCards().loadCards()) }
+        binding.photosLinear.adapter= this.context?.let { CardsAdapter(it,DataSetForCards().loadCards(), ::goToProfileFragment) }
+
         return binding.root
+    }
+    fun goToProfileFragment(profileId: Int) {
+        val action = CardsFragmentDirections.actionCardsFragmentToProfileFragment(profileId)
+        findNavController().navigate(action)
     }
 
 }
