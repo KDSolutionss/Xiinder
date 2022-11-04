2bc92be330e73043c635bf233d0b2a937f2662fa
Nazar Garipov <n.garipov@skbkontur.ru>
2022-11-04 21:15:03 +0500

    1 try

--- app/src/main/java/com/example/xiinder/network/ProfileInfo.kt
+++
@@ -1,5 +0,0 @@
-package com.example.xiinder.network
-
-import androidx.annotation.DrawableRes
-
-data class ProfileInfo(val profileId: Int, val name: String, val info: String, @DrawableRes val imageId:Int)
