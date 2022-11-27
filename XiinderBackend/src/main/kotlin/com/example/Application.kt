80d3bb2c0700bec9072ce1a2db199125aadb20e1
Nazar Garipov <nazar.garipov475@gmail.com>
2022-11-27 20:37:42 +0500

    регистрация пользователя

--- XiinderBackend/src/main/kotlin/com/example/Application.kt
+++ XiinderBackend/src/main/kotlin/com/example/Application.kt
@@ -1,12 +1,15 @@
 package com.example
 
+import com.example.database.DatabaseFactory
 import io.ktor.server.application.*
 import io.ktor.server.netty.*
 import com.example.plugins.*
 
 fun main(args: Array<String>): Unit = EngineMain.main(args)
 
+@Suppress("unused")
 fun Application.module() {
+    DatabaseFactory.init(environment.config)
     configureSecurity()
     configureRouting()
 }
--- XiinderBackend/src/main/kotlin/com/example/plugins/Security.kt
+++ XiinderBackend/src/main/kotlin/com/example/plugins/Security.kt
@@ -1,8 +1,8 @@
-
 package com.example.plugins
 
-import com.auth0.jwt.*
-import com.auth0.jwt.algorithms.*
+import com.auth0.jwt.JWT
+import com.auth0.jwt.algorithms.Algorithm
+import com.example.LoginService
 import io.ktor.http.*
 import io.ktor.serialization.kotlinx.json.*
 import io.ktor.server.application.*
@@ -12,11 +12,14 @@ import io.ktor.server.plugins.contentnegotiation.*
 import io.ktor.server.request.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
-import kotlinx.serialization.*
+import kotlinx.serialization.Serializable
 import java.util.*
 
 @Serializable
 data class User(val username: String, val password: String)
+
+@Serializable
+data class UserRegister(val username: String, val password: String)
 
 fun Application.configureSecurity() {
     install(ContentNegotiation) {
@@ -26,14 +29,17 @@ fun Application.configureSecurity() {
     val issuer = environment.config.property("jwt.issuer").getString()
     val audience = environment.config.property("jwt.audience").getString()
     val myRealm = environment.config.property("jwt.realm").getString()
+    val loginService = LoginService()
     install(Authentication) {
         jwt("auth-jwt") {
             realm = myRealm
-            verifier(JWT
-                .require(Algorithm.HMAC256(secret))
-                .withAudience(audience)
-                .withIssuer(issuer)
-                .build())
+            verifier(
+                JWT
+                    .require(Algorithm.HMAC256(secret))
+                    .withAudience(audience)
+                    .withIssuer(issuer)
+                    .build()
+            )
             validate { credential ->
                 if (credential.payload.getClaim("username").asString() != "") {
                     JWTPrincipal(credential.payload)
@@ -48,17 +54,35 @@ fun Application.configureSecurity() {
     }
 
     routing {
+        post("/register") {
+            val userRegister = call.receive<UserRegister>()
+            if (loginService.userExists(userRegister.username))
+                call.respond(HttpStatusCode.Conflict, "Username already registered")
+            else {
+                if (loginService.register(userRegister)) {
+                    val token = JWT.create()
+                        .withAudience(audience)
+                        .withIssuer(issuer)
+                        .withClaim("username", userRegister.username)
+                        .withExpiresAt(Date(System.currentTimeMillis() + 60000))
+                        .sign(Algorithm.HMAC256(secret))
+                    call.respond(HttpStatusCode.Created, hashMapOf("token" to token))
+                } else call.respond(HttpStatusCode.InternalServerError)
+            }
+        }
         post("/login") {
             val user = call.receive<User>()
-            // Check username and password
-            // ...
-            val token = JWT.create()
-                .withAudience(audience)
-                .withIssuer(issuer)
-                .withClaim("username", user.username)
-                .withExpiresAt(Date(System.currentTimeMillis() + 60000))
-                .sign(Algorithm.HMAC256(secret))
-            call.respond(hashMapOf("token" to token))
+            if (!loginService.isValidUserAuth(user.username, user.password))
+                call.respond(HttpStatusCode.Unauthorized, "Wrong username or password")
+            else {
+                val token = JWT.create()
+                    .withAudience(audience)
+                    .withIssuer(issuer)
+                    .withClaim("username", user.username)
+                    .withExpiresAt(Date(System.currentTimeMillis() + 60000))
+                    .sign(Algorithm.HMAC256(secret))
+                call.respond(hashMapOf("token" to token))
+            }
         }
 
         authenticate("auth-jwt") {
