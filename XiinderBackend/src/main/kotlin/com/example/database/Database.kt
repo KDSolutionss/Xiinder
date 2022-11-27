a1b2f67807a9ee62c96e6cf5eae1cce55afc5d41
Nazar Garipov <nazar.garipov475@gmail.com>
2022-11-27 20:48:41 +0500

    регистрация пользователя

---
+++ XiinderBackend/src/main/kotlin/com/example/database/Database.kt
@@ -0,0 +1,26 @@
+package com.example.database
+
+import com.example.models.UserLoginInfos
+import com.example.models.Users
+import io.ktor.server.config.*
+import kotlinx.coroutines.Dispatchers
+import org.jetbrains.exposed.sql.*
+import org.jetbrains.exposed.sql.transactions.experimental.newSuspendedTransaction
+import org.jetbrains.exposed.sql.transactions.transaction
+
+object DatabaseFactory {
+    fun init(config: ApplicationConfig){
+        val jdbcUrl = config.property("database.jdbcURL").getString()
+        val driverClassName = config.property("database.driverClassName").getString()
+        val username = config.property("database.username").getString()
+        val password = config.property("database.password").getString()
+        val database = Database.connect(jdbcUrl, driverClassName, username, password)
+        transaction(database) {
+            SchemaUtils.create(Users)
+            SchemaUtils.create(UserLoginInfos)
+        }
+    }
+
+    suspend fun <T> dbQuery(block: suspend () -> T): T =
+        newSuspendedTransaction(Dispatchers.IO) { block() }
+}
